5d5fce2411e7698bc5987d30e80ec3c9f4da36d6
Max Gordon <max@gforge.se>
2024-09-11 22:47:06 +0200

    Added language and cache to prompt reads as this will now change frequently

--- helpers/read_prompt.py
+++ helpers/read_prompt.py
@@ -11,7 +11,10 @@ class DualPrompt:
     human: str
 
 
-def read_single_prompt(name: str, prompt_path: Path) -> str:
+prompt_cache = {}
+
+
+def read_single_prompt(name: str, prompt_path: Path, language: str) -> str:
     """
     Read a single prompt from the provided path.
 
@@ -19,10 +22,23 @@ def read_single_prompt(name: str, prompt_path: Path) -> str:
     - name: Name of the prompt file to read.
     - prompt_path: Path to the prompt directory.
     """
+    key = f'{name}_{str(prompt_path)}_{language}'
+    if key in prompt_cache:
+        return prompt_cache[key]
+
     if not prompt_path.exists():
         raise FileNotFoundError(f"Prompt directory not found at {prompt_path}")
 
-    base = Path('./prompting') / prompt_path
+    language_path = prompt_path / language
+    if not language_path.exists():
+        if language.lower() == 'original':
+            language_path = prompt_path / "English"
+
+        if not language_path.exists():
+            raise FileNotFoundError(
+                f"Language directory not found at {language_path}")
+
+    base = Path('./prompting') / language_path
     path = base / (name + '.md')
     if not path.exists():
         raise FileNotFoundError(
@@ -31,19 +47,29 @@ def read_single_prompt(name: str, prompt_path: Path) -> str:
 
     try:
         with open(path, 'r', encoding='utf-8') as f:
-            return f.read()
+            prompt_txt = f.read()
+            prompt_cache[key] = prompt_txt
+            return prompt_txt
     except FileNotFoundError as e:
         msg = f'File not found for {name}, looking for {path}'
         raise FileNotFoundError(f"{str(e)}: {msg}") from e
 
 
-def read_dual_prompt(name: str, prompt_path: Path) -> DualPrompt:
+def read_dual_prompt(
+    name: str,
+    prompt_path: Path,
+    language: str,
+) -> DualPrompt:
     """
     Read a dual prompt from the provided path.
     """
     try:
-        system = read_single_prompt(name + '_system', prompt_path)
-        human = read_single_prompt(name + '_human', prompt_path)
+        system = read_single_prompt(name + '_system',
+                                    prompt_path=prompt_path,
+                                    language=language)
+        human = read_single_prompt(name + '_human',
+                                   prompt_path=prompt_path,
+                                   language=language)
         return DualPrompt(system, human)
     except FileNotFoundError as e:
         msg = f'File not found for {name}'
